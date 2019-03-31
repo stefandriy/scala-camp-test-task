af49d8724a1d05a218c9382e5d38bb89a7d83cf4
stefandriy <stefandriy@gmail.com>
2019-03-31 22:26:41 +0300

    do stuff with spark

--- build.sbt
+++ build.sbt
@@ -1,14 +1,16 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.12.8"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
+ThisBuild / scalaVersion := "2.12.8"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "com.example"
 ThisBuild / organizationName := "example"
 
 lazy val root = (project in file("."))
   .settings(
     name := "scala-camp-test-task",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0",
+    libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
   )
 
 // Uncomment the following for publishing to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,36 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import java.io.File
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions._
+import org.spark_project.guava.base.Strings.isNullOrEmpty
+
+import scala.collection.mutable
+
+object Hello extends App {
+
+  val conf = new SparkConf().setAppName("scala_test").setMaster("local[4]")
+  val spark = SparkSession.builder.config(conf).getOrCreate()
+
+  val path = "/path/to/crimes"
+  val files = new File(path).list.map(file => path + "/" + file)
+
+  spark.read.option("header", "true").csv(files: _*)
+    .filter(row => !isNullOrEmpty(row.getAs[String]("Crime ID")))
+    .filter(row => !isNullOrEmpty(row.getAs[String]("Longitude")))
+    .filter(row => !isNullOrEmpty(row.getAs[String]("Latitude")))
+    .groupBy("Longitude", "Latitude")
+    .agg(collect_list(
+      struct("Crime ID", "Month", "Reported by", "Falls within", "Longitude", "Latitude", "Location", "LSOA code", "LSOA name", "Crime type", "Last outcome category", "Context")),
+      count("*").as("count"))
+    .sort(desc("count"))
+    .limit(5)
+    .foreach(row => {
+      println("(" + row.getAs[String]("Longitude") + "," + row.getAs[String]("Latitude") + ")" + ": " + row.getAs[String]("count"))
+      row.getAs[mutable.WrappedArray[Row]](2)
+        .filter(row2 => row2.getAs[String]("Crime type").toLowerCase().contains("theft"))
+        .foreach(row2 => println(row2))
+    })
 }
